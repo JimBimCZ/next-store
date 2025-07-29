069ae92d7c2ba30e2a1fe4e5989505669e0abf24
vitbusek <busek.vit@gmail.com>
2025-07-29 11:58:26 +0200

    setup shadcn-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,22 @@
-import {Metadata} from "next";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Next Store',
     description: 'A nifty store built with Next.js',
 };
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import {NextPage} from "next";
-
+import { NextPage } from "next";
+import { Button } from "@/components/ui/button";
 
 const HomePage: NextPage = () => {
-  return <h1 className="text-3xl">Home Page</h1>
-}
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant="outline" size="lg" className="m-8 capitalize">
+        Click me
+      </Button>
+    </div>
+  );
+};
 
 export default HomePage;
