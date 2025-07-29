845669863dc4c9196ca968f65dd93b9db785ee22
vitbusek <busek.vit@gmail.com>
2025-07-29 10:19:38 +0200

    initial commit

---
+++ app/favourites/page.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from "next";
+
+const FavouritesPage: NextPage = () => {
+    return (<div>FavouritesPage</div>)
+}
+
+export default FavouritesPage;
---
+++ app/reviews/page.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from "next";
+
+const ReviewsPage: NextPage = () => {
+    return (<div>ReviewsPage</div>)
+}
+
+export default ReviewsPage
